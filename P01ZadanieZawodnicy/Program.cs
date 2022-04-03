26d3c1f5f3f8fdbd40fa5e692c80a37fd44750f6
Tomasz Leś <lest@ee.pw.edu.pl>
2022-04-03 09:22:49 +0200

    Add project files.

---
+++ P01ZadanieZawodnicy/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01ZadanieZawodnicy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sciezka = "http://tomaszles.pl/wp-content/uploads/2019/06/zawodnicy.txt";
+            ManagerZawodnikow mz = new ManagerZawodnikow(sciezka,TypImportu.Zdalny);
+            mz.WygenerujZawodnikow();
+
+            Console.WriteLine("kraj\tśrednia");
+            GrupaKraj[] gk = mz.PodajSredniWzrostDlaKazdegoKraju();
+            foreach (var g in gk)
+                Console.WriteLine(g.NazwaKraju + "\t" + g.SredniWzrost);
+
+            string podsuwamie = string.Format("Podsumowanie: średni wzrost wynosi: {0}" +
+                " a średnia waga wynosi {1}",
+                mz.PodajSredniWzrost(), mz.PodajSredniWage());
+
+            Console.WriteLine(podsuwamie);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ P01ZadanieZawodnicy/Zawodnik.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01ZadanieZawodnicy
+{
+    class Zawodnik
+    {
+        public int Id;
+        public int IdTrenera;
+        public string Imie;
+        public string Nazwisko;
+        public string Kraj;
+        public DateTime DataUrodzenia;
+        public int Wzrost;
+        public int Waga;
+
+        public string PrzedstawSie()
+        {
+            return $"Nazywam się {Imie} {Nazwisko} i pochodzę z {Kraj}";
+        }
+
+
+    }
+}
